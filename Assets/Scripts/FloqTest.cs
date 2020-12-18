f996bfd12ae1baa1c9e91a05bf85c67c8e706171
DenisVargasRivero <54225396+DenisVargasRivero@users.noreply.github.com>
2020-12-18 16:27:11 -0300

    Vector Avoidance!
    Reconstruí el vector de avoidance, hasta que el resultado quedo bastante satisfactorio!
    Hay algunos detalles que tenemos que tener en cuenta, pero en general, el resultado se ve muy bien!

--- Assets/Scripts/FloqTest.cs
+++ Assets/Scripts/FloqTest.cs
@@ -2,60 +2,182 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEditor;
 using IA.Floquing;
+using IA.LineOfSight;
+using System;
 
 public class FloqTest : MonoBehaviour
 {
-    public GameObject Lider = null;
-    public LayerMask floquingAgentsLM = ~0;
-    public LayerMask obstaclesAgents = ~0;
-    public float _floquingRadius = 1f;
-    public float _moveSpeed = 5f;
-    public float _minSeparationRadius = 2;
+    [SerializeField] LineOfSightComponent _sight = null;
+    [SerializeField] float _minimunAvoidanceAngle = 1f;
+    [SerializeField] float _minimunAvoidanceRadius = 2f;
+    [SerializeField] float _maximunAvoidanceRadius = 2f;
+    [SerializeField] AnimationCurve AvoidanceWeightModifier = AnimationCurve.Linear(0,0,1,1);
+
+    [SerializeField] Color angleColor = Color.white;
+
+    [SerializeField] GameObject Lider = null;
+    [SerializeField] LayerMask floquingAgentsLM = ~0;
+    [SerializeField] LayerMask obstaclesAgents = ~0;
+    [SerializeField] float _floqRadius = 1f;
+    [SerializeField] float _moveSpeed = 5f;
 
     Vector3 Alligment = Vector3.zero;
     Vector3 cohesion = Vector3.zero;
+    /// <summary>
+    /// Vector de Avoidance hacia mis aliados!
+    /// </summary>
     Vector3 separation = Vector3.zero;
+    /// <summary>
+    /// Vector de Avoidance hacia obstáculos.
+    /// </summary>
     Vector3 avoidance = Vector3.zero;
+    Vector3 EndResult = Vector3.zero;
 
     [Header("Weights")]
     [SerializeField] float SeparationWeight = 1f;
     [SerializeField] float cohetionWeight = 1f;
     [SerializeField] float avoidanceWeight = 1f;
 
-
     [Header("============= Debug =============")]
-    public List<Transform> debugAllies = new List<Transform>();
-    public List<Transform> obstacles = new List<Transform>();
-    public Color debug_AlligmentColor = new Color();
-    public Color debug_CohesionColor = new Color();
-    public Color debug_SeparationColor = new Color();
+    [SerializeField] List<Transform> debugAllies = new List<Transform>();
+    [SerializeField] List<Transform> obstacles = new List<Transform>();
+
+    [Header("Colors")]
+    [SerializeField] Color debug_FlockingDistanceColor = new Color();
+    [SerializeField] Color debug_AlligmentColor = new Color();
+    [SerializeField] Color debug_CohesionColor = new Color();
+    [SerializeField] Color debug_SeparationColor = new Color();
+    [SerializeField] Color debug_LeaderDirectionColor = new Color();
+    [SerializeField] Color debug_resultColor = new Color();
+
+    [Header("Display Settings")]
+    [SerializeField] bool ShowFlockRange = false;
+    [SerializeField] bool StayQuiet = false; //Esto lo podemos usar para bloquear el movimiento y ver el resultado de los cálculos.
+    [SerializeField] bool ShowAlligment = false;
+    [SerializeField] bool ShowCohesion = false;
+    [SerializeField] bool ShowSeparation = false;
+    [SerializeField] bool ShowLeaderDirection = false;
+    [SerializeField] bool ShowEndResult = false;
+
+    [Header("Avoidance")]
+    [SerializeField] bool ShowAvoidance = false;
+    [SerializeField] bool ShowMinimumAvoidanceRadius = false;
+    [SerializeField] bool ShowMaximunAvoidanceRadius = false;
+    [SerializeField] Color debug_AvoidanceColorLabel = new Color();
+    [SerializeField] Color debug_MinimunAvoidanceDistanceColor = new Color();
+    [SerializeField] Color debug_MaximunAvoidanceDistanceColor = new Color();
+
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
-        Gizmos.matrix = Matrix4x4.Scale(new Vector3(1, 0, 1));
-        Gizmos.DrawWireSphere(transform.position, _floquingRadius);
-
-        Gizmos.color = debug_AlligmentColor;
-        Gizmos.DrawLine(transform.position, transform.position + Alligment * 10);
-
-        Gizmos.color = debug_CohesionColor;
-        if (cohesion != Vector3.zero)
+        if (ShowFlockRange)
         {
+            Gizmos.color = debug_FlockingDistanceColor;
+            Gizmos.matrix = Matrix4x4.Scale(new Vector3(1, 0, 1));
+            Gizmos.DrawWireSphere(transform.position, _floqRadius); 
+        }
+        if (ShowLeaderDirection)
+        {
+            Gizmos.color = debug_LeaderDirectionColor;
+            Gizmos.DrawLine(transform.position, Lider.transform.position);
+
+            Vector3 Leader_labelPosition = getMedianPoint(transform.position, Lider.transform.position).YComponent(0 + 0.1f);
+            DrawLabel(Leader_labelPosition, "Lider direction", Color.white);
+        }
+
+        if (ShowAlligment)
+        {
+            Gizmos.color = debug_AlligmentColor;
+            Gizmos.DrawLine(transform.position, transform.position + Alligment);
+
+            Vector3 Alligment_LabelPos = getMedianPoint(transform.position, transform.position + cohesion);
+            Alligment_LabelPos.y += 0.1f;
+
+            DrawLabel(Alligment_LabelPos, $"Alligment -> m: {Alligment.magnitude}", Color.white);
+        }
+        if (ShowCohesion)
+        {
+            Gizmos.color = debug_CohesionColor;
             Gizmos.DrawLine(transform.position, transform.position + cohesion);
+            Vector3 cohesion_endPos = getMedianPoint(transform.position, transform.position + cohesion);
+            cohesion_endPos.y += 0.1f;
+
+            DrawLabel(cohesion_endPos, $"Cohesion -> m: {cohesion.magnitude}", Color.white);
         }
-        Gizmos.color = debug_SeparationColor;
-        if (cohesion != Vector3.zero)
+        if (ShowSeparation)
         {
+            Gizmos.color = debug_SeparationColor;
             Gizmos.DrawLine(transform.position, transform.position + separation);
+
+            Vector3 separation_medianPoint = getMedianPoint(transform.position, transform.position + separation);
+            separation_medianPoint.y = +0.2f;
+
+            DrawLabel(separation_medianPoint, $"Separation -> m: {separation.magnitude}", Color.white);
         }
+        if (ShowAvoidance)
+        {
+            Gizmos.color = debug_AvoidanceColorLabel;
+            Gizmos.DrawLine(transform.position, transform.position + avoidance);
+
+            Vector3 avoidance_medianPoint = getMedianPoint(transform.position, transform.position + avoidance);
+            avoidance_medianPoint.y = +0.2f;
+
+            DrawLabel(avoidance_medianPoint, $"Avoidance -> m: {avoidance.magnitude}", debug_AvoidanceColorLabel);
+
+            if (ShowMinimumAvoidanceRadius)
+            {
+                //Rango
+                Gizmos.color = debug_MinimunAvoidanceDistanceColor;
+                Gizmos.matrix = Matrix4x4.Scale(new Vector3(1, 0, 1));
+                Gizmos.DrawWireSphere(transform.position, _minimunAvoidanceRadius);
+
+                var currentPosition = transform.position;
+
+                //Ángulo
+                Gizmos.color = angleColor;
+                Gizmos.DrawLine(currentPosition, currentPosition + Quaternion.Euler(0, _minimunAvoidanceAngle + 1, 0) * transform.forward * _minimunAvoidanceRadius);
+                Gizmos.DrawLine(currentPosition, currentPosition + Quaternion.Euler(0, -_minimunAvoidanceAngle - 1, 0) * transform.forward * _minimunAvoidanceRadius);
+            }
+            if (ShowMaximunAvoidanceRadius)
+            {
+                Gizmos.color = debug_MaximunAvoidanceDistanceColor;
+                Gizmos.matrix = Matrix4x4.Scale(new Vector3(1, 0, 1));
+                Gizmos.DrawWireSphere(transform.position, _maximunAvoidanceRadius);
+            }
+        }
+
+        if (ShowEndResult)
+        {
+            Gizmos.color = debug_resultColor;
+            Gizmos.DrawLine(transform.position, transform.position + EndResult * 2);
+
+            Vector3 Alligment_LabelPos = getMedianPoint(transform.position, transform.position + EndResult);
+            Alligment_LabelPos.y += 0.1f;
+
+            DrawLabel(Alligment_LabelPos, $"End Restult: {EndResult.magnitude}", Color.white);
+        }
+    }
+
+    private Vector3 getMedianPoint(Vector3 from, Vector3 to)
+    {
+        return Vector3.Lerp(from, to, 0.5f);
+    }
+
+    private void DrawLabel(Vector3 labelPosition, string message, Color textColor)
+    {
+        GUIContent labelContent = new GUIContent();
+        labelContent.text = message;
+        GUIStyle style = new GUIStyle();
+        style.normal.textColor = textColor;
+        Handles.Label(labelPosition, labelContent, style);
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        debugAllies = getAllies();
+        //debugAllies = getAllies();
     }
 
     // Update is called once per frame
@@ -65,26 +187,33 @@ public class FloqTest : MonoBehaviour
         Vector3 dirToLeader = vecToLeader.normalized;
         float distToLeader = Vector3.Distance(transform.position, Lider.transform.position);
 
-        var closerEntities = Physics.OverlapSphere(transform.position, _floquingRadius, floquingAgentsLM); //chequeo mi entorno.
-
-        //vectores de floquing!!
-        //debugAllies = getCloseAllies(closerEntities).ToList();
-        obstacles = getCloserObstacles().ToList();
+        obstacles = _sight.GetAllVisibles(transform.position, _maximunAvoidanceRadius, _minimunAvoidanceAngle, obstaclesAgents);
 
         Alligment = transform.getAlignment(debugAllies);
         cohesion = transform.getCohesion(debugAllies, cohetionWeight).YComponent(0);
         separation = transform.getSeparation(debugAllies, SeparationWeight).YComponent(0);
-        avoidance = transform.getAvoidance(obstacles, avoidanceWeight).YComponent(0);
+        avoidance = transform.getAvoidance(obstacles, (position) => AvoidanceWeightModifier.Evaluate(position), _maximunAvoidanceRadius, _minimunAvoidanceRadius).YComponent(0);
 
-        //print("Avoidance is: " + avoidance);
-        //print(separation);
+        //EndResult = dirToLeader + separation + cohesion + avoidance;
+        EndResult = separation + cohesion + avoidance;
 
-        if (distToLeader > _floquingRadius)
+        if (StayQuiet)
+            return;
+
+        //La rotación depende del alligment solamente.
+
+        if (distToLeader > _floqRadius)
         {
-            Vector3 dirToMove = dirToLeader + separation + cohesion + avoidance;
+            //transform.forward = ((EndResult + Alligment).normalized).YComponent(0); //El componente forward debería ser algo que se actualiza en el tiempo.
+            transform.forward = Vector3.Slerp(transform.forward, EndResult.normalized.YComponent(0), 0.5f);
+            transform.position += (EndResult * _moveSpeed * Time.deltaTime);
+        }
 
-            transform.forward = ((dirToMove + Alligment).normalized).YComponent(0);
-            transform.position += (dirToMove * _moveSpeed * Time.deltaTime);
+        //Buscar una forma de hacer que se detenga.
+        if (distToLeader < 2)
+        {
+            //transform.forward = ((EndResult + Alligment).normalized).YComponent(0); //El componente forward debería ser algo que se actualiza en el tiempo.
+            transform.forward = Vector3.Slerp(transform.forward, ((EndResult + Alligment).normalized).YComponent(0), 0.5f) ; //El componente forward debería ser algo que se actualiza en el tiempo.
         }
     }
 
@@ -95,6 +224,7 @@ public class FloqTest : MonoBehaviour
             .Select(x => x.transform)
             .ToList();
     }
+
     /// <summary>
     /// Retorna los aliados mas cercanos para calcular el floquing.
     /// </summary>
@@ -106,16 +236,5 @@ public class FloqTest : MonoBehaviour
                     .Select(x => x.transform);
 
         return allies;
-    }
-    /// <summary>
-    /// Retorna los obstáculos más cercanos.
-    /// </summary>
-    public IEnumerable<Transform> getCloserObstacles()
-    {
-        var closerEntities = Physics.OverlapSphere(transform.position, _floquingRadius, obstaclesAgents); //chequeo mi entorno.
-
-        var obstacles = closerEntities
-                        .Select(x => x.transform);
-        return obstacles;
     }
 }
