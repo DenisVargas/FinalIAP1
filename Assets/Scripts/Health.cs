c862bc265d4db842c9f79d040f4f44d10492bdad
MarcosAM06 <40743539+MarcosAM06@users.noreply.github.com>
2020-12-20 15:26:58 -0300

    Barra de vida en proceso

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHealth = 100;
+
+    private int currentHealth;
+
+    public event Action<float> OnHealthPcChanged = delegate { };
+
+    private void OnEnable() 
+    {
+
+        currentHealth = maxHealth;
+      
+    }
+
+    public void ModifyHealth(int amount) 
+    {
+        currentHealth += amount;
+        float currentHealthPct = (float)currentHealth / (float)maxHealth;
+        OnHealthPcChanged = currentHealthPct;
+    }
+
+
+}
